4e8394e26cc0c44c6088e20eb3cff539bd1aa0ef
gunadermawan <gunadermawan.official@gmail.com>
2023-04-21 05:57:05 +0700

    sample of expanded and flexible widget

--- lib/expanded.dart
+++ lib/expanded.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Rainbow extends StatelessWidget {
@@ -37,6 +36,75 @@ class Rainbow extends StatelessWidget {
           color: Colors.purple,
         )),
       ],
+    );
+  }
+}
+
+class RainbowFlexibleWidget extends StatelessWidget {
+  const RainbowFlexibleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                ExpandedWidget(),
+                FlexibleWidget(),
+              ],
+            ),
+            Row(
+              children: const [ExpandedWidget(), ExpandedWidget()],
+            ),
+            Row(
+              children: const [FlexibleWidget(), FlexibleWidget()],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  const FlexibleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+        child: Container(
+      decoration: BoxDecoration(
+          color: Colors.tealAccent, border: Border.all(color: Colors.white)),
+      child: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          'flexible',
+          style: TextStyle(color: Colors.teal, fontSize: 24),
+        ),
+      ),
+    ));
+  }
+}
+
+class ExpandedWidget extends StatelessWidget {
+  const ExpandedWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.teal, border: Border.all(color: Colors.black)),
+        child: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'expanded',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+        ),
+      ),
     );
   }
 }
