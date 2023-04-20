64942c888c8e875f690600a4e3212cdb2ac29c56
gunadermawan <gunadermawan.official@gmail.com>
2023-04-21 05:24:30 +0700

    sample of expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_beginer/expanded.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: ScrollingScreen(),
+      home: const Rainbow(),
     );
   }
 }
@@ -55,29 +56,31 @@ class _FirstScreenState extends State<FirstScreen> {
   }
 }
 
-class ScrollingScreen extends StatelessWidget {
-  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: numberList.map((number) {
-          return Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: Center(
-              child: Text(
-                '$number', // Ditampilkan sesuai item
-                style: const TextStyle(fontSize: 50),
-              ),
-            ),
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
+// class ScrollingScreen extends StatelessWidget {
+//   final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: ListView.builder(
+//         itemCount: numberList.length,
+//         itemBuilder: (BuildContext context, int index),
+//         children: numberList.map((number) {
+//           return Container(
+//             height: 250,
+//             decoration: BoxDecoration(
+//               color: Colors.grey,
+//               border: Border.all(color: Colors.black),
+//             ),
+//             child: Center(
+//               child: Text(
+//                 '$number', // Ditampilkan sesuai item
+//                 style: const TextStyle(fontSize: 50),
+//               ),
+//             ),
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+// }
